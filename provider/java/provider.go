0dc7465c587b7c77fd1626f251db56d3eda5334e
Fabian von Feilitzsch <fabian@fabianism.us>
2022-11-21 13:03:42 -0500

    return symbolkind as extra
    Signed-off-by: Fabian von Feilitzsch <fabian@fabianism.us>

--- provider/java/provider.go
+++ provider/java/provider.go
@@ -101,31 +101,85 @@ func (p *javaProvider) Evaluate(cap string, conditionInfo []byte) (lib.ProviderE
 	incidents := []lib.IncidentContext{}
 	for _, s := range symbols {
 
-		if s.Kind == protocol.Module {
-			references := p.GetAllReferences(s)
-			for _, ref := range references {
-				// Look for things that are in the location loaded, //Note may need to filter out vendor at some point
-				if strings.Contains(ref.URI, p.config.Location) {
-					incidents = append(incidents, lib.IncidentContext{
-						FileURI: ref.URI,
-						Extras: map[string]interface{}{
-							"lineNumber": ref.Range.Start.Line,
-						},
-					})
-				}
+		// if s.Kind == protocol.Module {
+		references := p.GetAllReferences(s)
+		for _, ref := range references {
+			// Look for things that are in the location loaded, //Note may need to filter out vendor at some point
+			if strings.Contains(ref.URI, p.config.Location) {
+				incidents = append(incidents, lib.IncidentContext{
+					FileURI: ref.URI,
+					Extras: map[string]interface{}{
+						"lineNumber": ref.Range.Start.Line,
+						// TODO(fabianvf) remove this, Temporary for testing purpses
+						"kind": symbolKindToString(s.Kind),
+					},
+				})
 			}
 		}
 	}
 
-	if len(incidents) == 0 {
-		return lib.ProviderEvaluateResponse{
-			Passed: true,
-		}, nil
-	}
 	return lib.ProviderEvaluateResponse{
-		Passed:    false,
+		Passed:    len(incidents) == 0,
 		Incidents: incidents,
 	}, nil
+}
+
+func symbolKindToString(symbolKind protocol.SymbolKind) string {
+	switch symbolKind {
+	case 1:
+		return "File"
+	case 2:
+		return "Module"
+	case 3:
+		return "Namespace"
+	case 4:
+		return "Package"
+	case 5:
+		return "Class"
+	case 6:
+		return "Method"
+	case 7:
+		return "Property"
+	case 8:
+		return "Field"
+	case 9:
+		return "Constructor"
+	case 10:
+		return "Enum"
+	case 11:
+		return "Interface"
+	case 12:
+		return "Function"
+	case 13:
+		return "Variable"
+	case 14:
+		return "Constant"
+	case 15:
+		return "String"
+	case 16:
+		return "Number"
+	case 17:
+		return "Boolean"
+	case 18:
+		return "Array"
+	case 19:
+		return "Object"
+	case 20:
+		return "Key"
+	case 21:
+		return "Null"
+	case 22:
+		return "EnumMember"
+	case 23:
+		return "Struct"
+	case 24:
+		return "Event"
+	case 25:
+		return "Operator"
+	case 26:
+		return "TypeParameter"
+	}
+	return ""
 }
 
 func (p *javaProvider) Init(ctx context.Context, log logr.Logger) error {
